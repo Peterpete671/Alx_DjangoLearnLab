a3db6e9befa6706d98797a654981997c7bafce8b
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-11-09 15:27:40 +0300

    Fix views and URL patterns to meet task requirements

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
-from .models import Book, Library
-
-# Create your views here.
-#Function-based view to list all books
-def list_books(request):
-	books = Book.objects.all()
-	return render(request, 'list_books.html', {'books': books})
-
-from django.vies.generic import DetailView
+from django.http import HttpResponse
+from .models import Book
+from django.views.generic import DetailView
 from .models import Library
 
-#Class-based view to show library details
+# Function-based view: simple text list of books
+def list_books(request):
+    books = Book.objects.all()
+    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
+
+
+# Class-based view: library detail using DetailView
 class LibraryDetailView(DetailView):
-	model = Library
-	template_name = 'library_detail.html'
-	context_object_name = 'library'
+    model = Library
+    template_name = 'library_detail.html'  # template can remain for HTML output
+    context_object_name = 'library'
