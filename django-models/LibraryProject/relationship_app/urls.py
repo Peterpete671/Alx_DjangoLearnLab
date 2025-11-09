c0e0acdca007f8a7e748389a857653318fb8b09e
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-11-09 16:33:38 +0300

    Add user authentication: registration, login, and logout views with templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,3 +6,13 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+from django.urls import path
+from .views import register_view, CustomLoginView, CustomLogoutView
+
+urlpatterns = [
+    path('register/', register_view, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+]
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,3 +15,32 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
+from .forms import RegisterForm
+
+def register_view(request):
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			return redirect('home')
+		else:
+			form = RegisterForm()
+		return render(request, 'relationship_app/register.html', {'form': form})
+
+#Login view
+class CustomLoginView(LoginView):
+	template_name = 'relationship_app/login.html'
+
+#Logout view
+class CustomLogoutView(LogoutView):
+	template_name = 'relationship_app/logout.html'
