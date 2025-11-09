d875a5332ad4463d4b829248d700b8ea33f7fc0b
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-11-09 15:55:15 +0300

    Add required views, URLs, and template for relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Book, Library
-from django.views.generic import DetailView
 
-# --- Function-based view ---
+# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'relationship_app/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
-# --- Class-based view ---
-class LibraryDetailView(DetailView):
-    model = Library
+# Class-based view: list all books in a specific library
+class LibraryBookListView(ListView):
+    model = Book
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        # Get the library id from URL
+        library_id = self.kwargs.get('pk')
+        return Book.objects.filter(library_id=library_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['library'] = Library.objects.get(pk=self.kwargs.get('pk'))
+        return context
