83084e665a469bebff2060abd7870e2d3ff4e8cb
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-11-16 13:02:26 +0300

    creating a custom user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -8,3 +8,10 @@ class Book(models.Model):
 
 	def __str__(self):
 		return f"{self.title} by {self.author} ({self.publication_year})"
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
