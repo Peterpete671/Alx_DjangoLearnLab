c0e1cb770897ba327d712ef9c250a195d68aad42
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-12-07 14:19:33 +0300

    Adding comment urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,8 +20,22 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 
-    path("post/<int:pk>/comment/new/", CommentCreateView.as_view(), name="comment-create"),
-    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
-    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+    from django.urls import path
+from . import views
+
+app_name = "blog"
+
+urlpatterns = [
+    # Post CRUD URLs
+    path("posts/", views.PostListView.as_view(), name="post-list"),
+    path("post/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
+
+    path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment-create"),
+    path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment-update"),
+]
+,
 ]
 
