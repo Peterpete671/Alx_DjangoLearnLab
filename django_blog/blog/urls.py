bba4e1beec087ec53585ba5f38c334e47ce0a817
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-12-07 15:13:01 +0300

    Implement tagging and search functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,22 +1,22 @@
+# blog/urls.py
 from django.urls import path
 from . import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    # Post URLs...
-    path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    # Post CRUD URLs
+    path("", views.PostListView.as_view(), name="post_list"),
+    path("post/new/", views.PostCreateView.as_view(), name="post_create"),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
 
-    # Comment URLs...
-    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+    # Comment URLs
+    path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment_create"),
+    path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment_update"),
+    path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
 
-    # Tagging & search URLs
-    path('tags/<slug:tag_slug>/', views.TaggedPostListView.as_view(), name='tagged_posts'),
-    path('search/', views.SearchResultsView.as_view(), name='search_results'),
+    # Posts by tag
     path("tags/<str:tag_name>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
 ]
