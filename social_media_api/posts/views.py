eb8e763ddf989a85ab0b7f0735c80d4dcae02e5d
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-12-14 13:54:24 +0300

    Immplementing the like and notifications functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, permissions, filters, status, generics
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post, Comment
@@ -231,38 +231,48 @@ class FeedView(generics.ListAPIView):
         following_users = user.following.all()
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
     
-@action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
-def like(self, request, pk=None):
-    post = self.get_object()
-    user = request.user
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def like_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
 
-    like, created = Like.objects.get_or_create(user=user, post=post)
+    like, created = Like.objects.get_or_create(
+        user=request.user,
+        post=post
+    )
 
     if not created:
         return Response(
-            {"detail": "You have already liked this post"},
+            {"detail": "You have already liked this post."},
             status=400
         )
-    
-    if post.author != user:
-        Notification.object.create(
+
+    if post.author != request.user:
+        Notification.objects.create(
             recipient=post.author,
-            actor=user,
+            actor=request.user,
             verb="liked your post",
             target=post
-    )
-    return Response({"detail": "post liked successfully"}, status=201)
+        )
 
-@action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
-def unlike(self, request, pk=None):
-    post = self.get_object()
-    user = request.user
+    return Response({"detail": "Post liked successfully."}, status=201)
 
-    like = Like.objects.filter(user=user, post=post).first()
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unlike_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    like = Like.objects.filter(
+        user=request.user,
+        post=post
+    ).first()
+
     if not like:
         return Response(
             {"detail": "You have not liked this post."},
             status=400
         )
+
     like.delete()
     return Response({"detail": "Post unliked successfully."}, status=200)
